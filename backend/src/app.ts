c0ced8fa25f95997ccf61a6662f07e92602727c6
nivregev <nivregev2@gmail.com>
2023-09-06 18:58:01 +0300

    Express error handling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,29 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import NoteModel from "./models/node";
 
 const app = express();
 
-app.get("/", async (req, res) => {
-  const notes = await NoteModel.find().exec();
-  res.status(200).json(notes);
+app.get("/", async (req, res, next) => {
+  try {
+    // throw Error("niv alert stop!");
+    const notes = await NoteModel.find().exec();
+    res.status(200).json(notes);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((req, res, next) => {
+  next(Error("Endpoint not found"));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  let errorMessage = "An Unknown Error Message";
+  if (error instanceof Error) errorMessage = error.message;
+  res.status(500).json({ error: errorMessage });
 });
 
 export default app;
