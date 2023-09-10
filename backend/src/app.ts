c3c11291bd66dabb11924cb58a8b79ed5a1713f1
nivregev <nivregev2@gmail.com>
2023-09-10 10:53:24 +0300

    Creating Http Errors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,14 +13,16 @@ app.use(express.json());
 app.use("/api/notes", NotesRoutes);
 
 app.use((req, res, next) => {
-  next(createHttpError("Endpoint not found"));
+  next(createHttpError(404, "Endpoint not found"));
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   console.error(error);
+
   let errorMessage = "An Unknown Error Message";
   let statusCode = 500;
+
   if (isHttpError(error)) {
     statusCode = error.status;
     errorMessage = error.message;
