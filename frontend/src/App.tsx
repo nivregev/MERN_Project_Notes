adb19b18eb229aa5ea3f369cff7f8b46fef96ebd
nivregev <nivregev2@gmail.com>
2023-09-11 10:48:29 +0300

    Running Backend and Frontend , Proxy(package.json)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Button } from "react-bootstrap";
+import { Note } from "./models/notes";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>hello</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  useEffect(() => {
+    async function loadNotes() {
+      try {
+        const response = await fetch("/api/notes", { method: "GET" });
+        const notes = await response.json();
+        setNotes(notes);
+      } catch (error) {
+        console.error(error);
+        alert(error);
+      }
+    }
+    loadNotes();
+  }, []);
+
+  return <div className="App">{JSON.stringify(notes)}</div>;
 }
 
 export default App;
---
+++ frontend/src/models/notes.ts
@@ -0,0 +1,7 @@
+export interface Note {
+  _id: string;
+  title: string;
+  text?: string;
+  createdAr: string;
+  updatedAt: string;
+}
