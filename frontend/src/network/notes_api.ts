d1cfe48d154521ba7bc4f5f5086d6d313a3f99db
nivregev <nivregev2@gmail.com>
2023-09-12 10:53:44 +0300

    adding createNote function

--- frontend/src/network/notes_api.ts
+++ frontend/src/network/notes_api.ts
@@ -15,3 +15,19 @@ export async function fetchNotes(): Promise<Note[]> {
   const response = await fetchData("/api/notes", { method: "GET" });
   return response.json();
 }
+
+export interface NoteInput {
+  title: string;
+  text?: string;
+}
+
+export async function createNote(note: NoteInput): Promise<Note> {
+  const response = await fetchData("/api/notes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(note),
+  });
+  return response.json();
+}
