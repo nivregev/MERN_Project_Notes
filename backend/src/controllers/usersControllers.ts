666706c547e1850cdcd1254b7a1998aa4a3bb6fd
nivregev <nivregev2@gmail.com>
2023-09-20 19:30:10 +0300

    authentication process is now working

--- backend/src/controllers/usersControllers.ts
+++ backend/src/controllers/usersControllers.ts
@@ -3,6 +3,22 @@ import createHttpError from "http-errors";
 import userModels from "../models/userModels";
 import bcrypt from "bcrypt";
 
+export const getAuthenticatedUser: RequestHandler = async (req, res, next) => {
+  const authenticatedUser = req.session.userId;
+  try {
+    if (!authenticatedUser) {
+      throw createHttpError(401, "User not authenticated");
+    }
+    const user = await userModels
+      .findById(authenticatedUser)
+      .select("+userEmail")
+      .exec();
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
 interface SignUpBody {
   userName?: string;
   userEmail?: string;
@@ -101,4 +117,14 @@ export const login: RequestHandler<
   } catch (error) {
     next(error);
   }
+};
+
+export const logout: RequestHandler = (req, res, next) => {
+  req.session.destroy((error) => {
+    if (error) {
+      next(error);
+    } else {
+      res.sendStatus(200);
+    }
+  });
 };
--- backend/src/routes/usersRoutes.ts
+++ backend/src/routes/usersRoutes.ts
@@ -3,8 +3,11 @@ import * as UserControllers from "../controllers/usersControllers";
 
 const router = express.Router();
 
+router.get("/", UserControllers.getAuthenticatedUser);
+
 router.post("/signup", UserControllers.signUp);
 
 router.post("/login", UserControllers.login);
+router.post("/logout", UserControllers.logout);
 
 export default router;
