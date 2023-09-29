7d47a155e791ede1e750942ca7171e348ff5da0f
nivregev <nivregev2@gmail.com>
2023-09-29 17:13:57 +0300

    adding some new HttpErrors of my own

--- frontend/src/network/notes_api.ts
+++ frontend/src/network/notes_api.ts
@@ -1,3 +1,4 @@
+import { ConflictError, UnauthorizedError } from "../errors/http_errors";
 import { Note } from "../models/notes";
 import { User } from "../models/users";
 
@@ -8,6 +9,19 @@ async function fetchData(input: RequestInfo, init?: RequestInit) {
   } else {
     const errorBody = await response.json();
     const errorMessage = errorBody.error;
+    if (response.status === 401) {
+      throw new UnauthorizedError(errorMessage);
+    } else if (response.status === 409) {
+      throw new ConflictError(errorMessage);
+    } else {
+      throw Error(
+        "Request failed with status: " +
+          response.status +
+          "message: " +
+          errorMessage
+      );
+    }
+
     throw Error(errorMessage);
   }
 }
